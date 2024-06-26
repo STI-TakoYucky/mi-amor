3afd6d037b9858594cb06cf18a6c85b0c43a7e93
Lucky Estrada <estrada.lnp@gmail.com>
2024-06-26 23:05:09 +0800

    almost done hobbies page

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const introbttn = document.getElementById("intro-next");
+
+try{
+    const introbttn = document.getElementById("intro-next");
 const firstPage = document.getElementById("firstPage");
 const secondPage = document.getElementById("secondPage");
 
@@ -16,3 +18,75 @@ introbttn.addEventListener('click', () => {
             introbttn.classList.remove("fa-chevron-left");
         }
 });
+} catch (err){
+    console.log(err);
+}
+
+
+
+
+let cards = [
+    {
+        title: "Reading",
+        img: "/images/h2.png",
+        desc: "Reading stories is my favorite thing to do whether I am busy or not. I read stories every day. It started when the pandemic happened.",
+        link: "my reading list:"
+    },
+    {
+        title: "Watching",
+        img: "/images/h3.png",
+        desc: "I love to watch series, especially Grey’s Anatomy.",
+        link: ""
+    },
+    {
+        title: "Listening Music!!",
+        img: "/images/h1.png",
+        desc: "Music is my comfort. It makes my heart happy.",
+        link: "my playlist:"
+    }
+];
+
+const hobbyContainer = document.querySelector(".hobbies-container");
+
+hobbyContainer.addEventListener('click', (e) => {
+    const body = document.body;
+   
+    if (e.target.classList.contains('hob-img')) {
+        const id = e.target.id;
+        let card;
+        
+        if (id === "readingCard") {
+            card = cards[0];
+        } else if (id === "watchingCard") {
+            card = cards[1];
+        } else if (id === "musicCard") {
+            card = cards[2];
+        }
+        
+        if (card) {
+            const modalHtml = displayModal(card.title, card.img, card.desc, card.link);
+            body.insertAdjacentHTML('beforeend', modalHtml);
+            hobbyContainer.classList.add("mainBlur");
+        }
+    } else {
+        console.log("Clicked element is not a hobby image");
+    }
+});
+
+function displayModal(title, img, desc, link) {
+    return `<article class="hobby-modal">
+        <div>
+            <div class="modal-header">
+                <h1>${title}</h1>
+                <i class="fa-solid fa-circle-xmark"></i>
+            </div>
+            <div class="modal-container">
+                <div class="modal-img-container"><img src="${img}" alt=""></div>
+                <div class="modal-desc">
+                    <div><p>${desc}</p></div>
+                    <div><p>${link}</p></div>
+                </div>
+            </div>
+        </div>
+    </article>`;
+}
