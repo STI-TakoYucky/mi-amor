57105f6f15670bef4cc4a3076872372fd2554758
Lucky Estrada <estrada.lnp@gmail.com>
2024-06-30 00:36:27 +0800

    fonts done

--- script.js
+++ script.js
@@ -23,82 +23,114 @@ introbttn.addEventListener('click', () => {
 }
 
 
-
-
-let cards = [
-    {
-        title: "Reading",
-        img: "/images/h2.png",
-        desc: "Reading stories is my favorite thing to do whether I am busy or not. I read stories every day. It started when the pandemic happened.",
-        link: "my reading list:"
-    },
-    {
-        title: "Watching",
-        img: "/images/h3.png",
-        desc: "I love to watch series, especially Grey’s Anatomy.",
-        link: ""
-    },
-    {
-        title: "Listening Music!!",
-        img: "/images/h1.png",
-        desc: "Music is my comfort. It makes my heart happy.",
-        link: "my playlist:"
-    }
-];
-
-const hobbyContainer = document.querySelector(".hobbies-container");
-
-hobbyContainer.addEventListener('click', (e) => {
-    const body = document.body;
-   
-    if (e.target.classList.contains('hob-img')) {
-        const id = e.target.id;
-        let card;
-        
-        if (id === "readingCard") {
-            card = cards[0];
-        } else if (id === "watchingCard") {
-            card = cards[1];
-        } else if (id === "musicCard") {
-            card = cards[2];
+try {
+    let cards = [
+        {
+            title: "Reading",
+            img: "/images/h2.png",
+            desc: "Reading stories is my favorite thing to do whether I am busy or not. I read stories every day. It started when the pandemic happened.",
+            link: "my reading list:"
+        },
+        {
+            title: "Watching",
+            img: "/images/h3.png",
+            desc: "I love to watch series, especially Grey’s Anatomy.",
+            link: ""
+        },
+        {
+            title: "Listening Music!!",
+            img: "/images/h1.png",
+            desc: "Music is my comfort. It makes my heart happy.",
+            link: "my playlist:"
         }
-        
-        if (card) {
-            const modalHtml = displayModal(card.title, card.img, card.desc, card.link);
-            body.insertAdjacentHTML('beforeend', modalHtml);
-            hobbyContainer.classList.add("mainBlur");
-
-            const modalExit = document.getElementById("modal-exit-bttn");
-
-            modalExit.addEventListener('click', () => {
-                const modal = document.querySelector(".hobby-modal");
-                hobbyContainer.classList.remove("mainBlur");
-                body.removeChild(modal);
-            });
-        }
-    } else {
-        console.log("Clicked element is not a hobby image");
-    }
-});
-
-
-
-function displayModal(title, img, desc, link) {
+    ];
     
-    return `<article class="hobby-modal">
-        <div>
-            <div class="modal-header">
-                <h1>${title}</h1>
-                <i class="fa-solid fa-circle-xmark" id="modal-exit-bttn"></i>
-            </div>
-            <div class="modal-container">
-                <div class="modal-img-container"><img src="${img}" alt=""></div>
-                <div class="modal-desc">
-                    <div><p>${desc}</p></div>
-                    <div><p>${link}</p></div>
+    const hobbyContainer = document.querySelector(".hobbies-container");
+    let active = false;
+    hobbyContainer.addEventListener('click', (e) => {
+        const body = document.body;
+        
+       
+        if (e.target.classList.contains('hob-img') && !active) {
+            active = true;
+            const id = e.target.id;
+            let card;
+            
+            if (id === "readingCard") {
+                card = cards[0];
+       
+            } else if (id === "watchingCard") {
+                card = cards[1];
+       
+            } else if (id === "musicCard") {
+                card = cards[2];
+                
+            }
+            
+            if (card) {
+                const modalHtml = displayModal(card.title, card.img, card.desc, card.link);
+                active = true;
+                body.insertAdjacentHTML('beforeend', modalHtml);
+                hobbyContainer.classList.add("mainBlur");
+                
+    
+                const modalExit = document.getElementById("modal-exit-bttn");
+    
+                modalExit.addEventListener('click', () => {
+                    const modal = document.querySelector(".hobby-modal");
+                    hobbyContainer.classList.remove("mainBlur");
+                    body.removeChild(modal);
+                    active = false;
+                });
+            }
+        } else {
+            console.log("Clicked element is not a hobby image");
+        }
+    });
+    
+    
+    
+    function displayModal(title, img, desc, link) {
+        
+        return `<article class="hobby-modal">
+            <div>
+                <div class="modal-header">
+                    <h1>${title}</h1>
+                    <i class="fa-solid fa-circle-xmark" id="modal-exit-bttn"></i>
+                </div>
+                <div class="modal-container">
+                    <div class="modal-img-container"><img src="${img}" alt=""></div>
+                    <div class="modal-desc">
+                        <div><p>${desc}</p></div>
+                        <div><p>${link}</p></div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </article>`;
+        </article>`;
+    }
+    
+    
+} catch (error) {
+    
 }
 
+
+
+try {
+    const menuBttn = document.getElementById('home-page-bttn');
+    const mobileMenu = document.querySelector('.mobile-nav');
+    const close = document.querySelector('.mobile-nav-close')
+
+    menuBttn.addEventListener('click', () => {
+        mobileMenu.classList.add("activeMenu");
+        console.log("HELLO");
+    })
+
+    close.addEventListener('click', () => {
+        if (mobileMenu.classList.contains("activeMenu")) {
+            mobileMenu.classList.remove("activeMenu")
+        }
+    })
+} catch (err) {
+    
+}
